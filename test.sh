7c0f2d1cbf2aefd2a9da612ded277ba251105ca7
AntonUrbanovich <antonurbanovich@mail.ru>
2022-08-17 19:05:12 +0300

    creating the changes for testing pull request using git console

--- test.sh
+++ test.sh
@@ -2,4 +2,5 @@
     sudo amazon-linux-extras install -y nginx1
     sudo systemctl enable nginx
     sudo systemctl start nginx
+    sudo systemctl status nginx
     echo "<html><body><p align=center><h1><strong>WELCOME TO MORDOR</strong> </h1></p></body></html>" > /usr/share/nginx/html/index.html
